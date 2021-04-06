8f8839a18eae2a2f031b9c4cd2cd0af3c0d9d647
Joey <joey.s.hornsby@gmail.com>
2021-04-06 13:20:13 +0100

    green on withdraw method

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -5,9 +5,16 @@ describe Account do
     it { is_expected.to respond_to :balance }
 
     describe "#deposit" do
-    # edge cases to come back to here. trying to withdraw money when there isn't enough.
+    #edge cases - someone tries to deposit something that isnt a number
         it 'adds money to the balance' do
             expect { subject.deposit(1000) }.to change { subject.balance }.by 1000
+        end
+    end
+
+    describe "#withdraw" do
+    # edge cases to come back to here. trying to withdraw money when there isn't enough. or do we want an overdraft function?
+        it 'removes money from the balance' do
+            expect { subject.withdraw(500) }.to change { subject.balance }.by -500
         end
     end
 end
