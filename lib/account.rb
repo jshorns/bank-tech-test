1d24eae223013c05c0cd8f225459145f78cc347b
Joey <joey.s.hornsby@gmail.com>
2021-04-06 13:12:14 +0100

    green on deposit method for bank account

--- lib/account.rb
+++ lib/account.rb
@@ -3,4 +3,9 @@ class Account
     def initialize
         @balance = 0
     end
+
+    def deposit(amount)
+        @balance += amount
+    end
+
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -4,4 +4,10 @@ describe Account do
 
     it { is_expected.to respond_to :balance }
 
+    describe "#deposit" do
+    # edge cases to come back to here. trying to withdraw money when there isn't enough.
+        it 'adds money to the balance' do
+            expect { subject.deposit(1000) }.to change { subject.balance }.by 1000
+        end
+    end
 end
