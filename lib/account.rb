bab8b4920f0d766eb0cb2b3dd328a574b0e2cbdf
Joey <joey.s.hornsby@gmail.com>
2021-04-08 12:26:47 +0100

    refactored balance to refer to previous transaction rather than storing balance separately, also made default opening balance into a constant

--- lib/account.rb
+++ lib/account.rb
@@ -6,17 +6,23 @@ require 'date'
 
 # Account class, for recording balance and making deposits and withdrawals.
 class Account
-  attr_reader :balance
+  OPENING_BALANCE = 0
 
   def initialize(transaction_history = TransactionHistory.new)
-    @balance = 0
     @transaction_history = transaction_history
+  end
+
+  def balance
+    if first_transaction?
+      OPENING_BALANCE
+    else
+      @transaction_history.transactions.last.balance_after
+    end
   end
 
   def deposit(amount)
     amount = check_format(amount)
     zero_value_transaction_error(amount)
-    @balance += amount
     record_transaction(amount)
   end
 
@@ -24,7 +30,6 @@ class Account
     amount = check_format(amount)
     zero_value_transaction_error(amount)
     check_balance(amount)
-    @balance -= amount
     record_transaction(-amount)
   end
 
@@ -34,9 +39,13 @@ class Account
 
   private
 
+  def first_transaction?
+    @transaction_history.transactions.length == 0
+  end
+
   def record_transaction(amount, transaction = Transaction.new)
     transaction.amount = amount
-    transaction.balance_after = @balance
+    transaction.balance_after = balance + amount
     @transaction_history.add_transaction(transaction)
   end
 
@@ -51,7 +60,8 @@ class Account
   end
 
   def check_balance(amount)
-    fail 'You do not have sufficient funds for that transaction' unless @balance - amount >= 0
+    return if first_transaction?
+    fail 'You do not have sufficient funds for that transaction' unless @transaction_history.transactions.last.balance_after - amount >= 0
   
   end
 end
